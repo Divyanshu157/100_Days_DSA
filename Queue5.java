991f633f2dcfc68b572896201bf353f19e8aa5f3
divyanshu sharma <107141300+Divyanshu157@users.noreply.github.com>
2023-11-13 20:19:26 +0530

    day20 Implemnt stack Annd Queue using Deque

---
+++ Queue5.java
@@ -0,0 +1,106 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Queue5 {
+
+
+//    problem 1
+//    Implement Stack Using Deque
+
+
+//    static class Queue{
+////        Stack using Queue
+//        Deque<Integer> deque = new LinkedList<>();
+//        public  void add(int data){
+//            deque.addLast(data);
+//        }
+//        public int remove(){
+//            return deque.removeLast();
+//
+//        }
+//        public int peek()
+//        {
+//            return deque.getLast();
+//
+//        }
+//        public boolean isEmpty()
+//        {
+//            return deque.isEmpty();
+//        }
+//
+//
+//
+//    }
+
+
+
+//    problem 2
+
+
+//    Implement queue using Deque
+
+//    static class Queue{
+//        Deque<Integer> deque= new LinkedList<>();
+//
+//        public void add(int data){
+//            deque.addLast(data);
+//        }
+//        public int remove(){
+//            return deque.removeFirst();
+//
+//        }
+//        public int peek(){
+//            return deque.getFirst();
+//        }
+//        public boolean isEmpty(){
+//            return deque.isEmpty();
+//
+//        }
+//    }
+
+
+    public static void main(String[] args) {
+
+//        Deque<Integer> deque = new LinkedList<>();
+//        deque.addFirst(2);
+//        deque.addFirst(1);
+//        deque.addLast(3);
+//        deque.addLast(4);
+//        deque.removeFirst();
+//
+//        System.out.println(deque);
+//        while (!deque.isEmpty()){
+//            System.out.print(deque.remove());
+//        }
+
+
+
+//        Queue q = new Queue();
+//        q.add(1);
+//        q.add(2);
+//        q.add(3);
+//
+//        while (!q.isEmpty()){
+//            System.out.print(q.peek()+" ");
+//            q.remove();
+//
+//        }
+
+        
+
+
+//
+//        Queue q = new Queue();
+//        q.add(1);
+//        q.add(2);
+//        q.add(3);
+//
+//        while (!q.isEmpty()){
+//            System.out.print(q.remove());
+//        }
+
+
+
+    }
+}
+
