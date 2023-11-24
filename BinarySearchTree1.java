395b45739bd7f4b24b9646697468428cfd2af3b2
divyanshu sharma <107141300+Divyanshu157@users.noreply.github.com>
2023-11-24 15:24:42 +0530

    Day31 Problem of BST
    Some extra problems of BST after Implementation

--- BinarySearchTree1.java
+++ BinarySearchTree1.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BinarySearchTree1 {
 
 //    what is BST?
@@ -92,6 +94,65 @@ public class BinarySearchTree1 {
     }
 
 
+//    Day 31 Problems of BST
+    public static void printRange(Node root , int k1 , int k2){
+        if (root==null){
+            return;
+        }
+        if(root.data>=k1 && root.data<=k2){
+            printRange(root.left,k1,k2);
+            System.out.print(root.data+" ");
+            printRange(root.right,k1,k2);
+        }else if (root.data<k1){
+            printRange(root.left,k1,k2);
+        }else {
+            printRange(root.right,k1,k2);
+        }
+
+    }
+
+//    problem 7
+
+    public static void printRootToLeaf(Node root, ArrayList<Integer> path ){
+
+        if (root==null){
+            return;
+        }
+        path.add(root.data);
+
+        if (root.left==null && root.right==null){
+            printPath(path);
+
+        }
+        printRootToLeaf(root.left,path);
+        printRootToLeaf(root.right,path);
+        path.remove(path.size()-1);
+
+    }
+    public static void printPath(ArrayList<Integer> path){
+        for (int i=0;i<path.size();i++){
+            System.out.print(path.get(i)+"->");
+        }
+        System.out.print("Null");
+        System.out.println();
+    }
+//
+//problem 8
+
+    public static boolean isValidBST(Node root, Node min , Node max){
+        if (root == null) {
+
+            return true;
+        }
+        if (min!=null && root.data<=min.data){
+            return false;
+        } else if (max!=null && root.data>=max.data){
+            return false;
+        }
+        return isValidBST(root.left , min,root) && isValidBST(root.right ,root,max);
+
+
+    }
     public static void main(String[] args) {
         int values[] ={ 8,5,3,1,4,6,10,11,14};
         Node root= null;
@@ -99,15 +160,17 @@ public class BinarySearchTree1 {
         for (int i=0;i<values.length;i++){
            root = insert(root ,values[i]);
         }
-        delete(root,1);
-        delete(root,10);
+//        delete(root,1);
+//        delete(root,10);
 
         inOrder(root);
+        System.out.println();
+//        printRange(root,5,12);
 
 //        System.out.println(search(root,5));
+//        printRootToLeaf(root,new ArrayList<>());
 
-
-
+        System.out.println(isValidBST(root,null,null));
 
     }
 
