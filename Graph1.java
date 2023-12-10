ffd53ac3ef0c0056f6224bc6e39883e0cfd6deb3
divyanshu sharma <107141300+Divyanshu157@users.noreply.github.com>
2023-12-10 17:35:06 +0530

    day 47 graph Traversal DFS,bfs, hasPath

--- Graph1.java
+++ Graph1.java
@@ -73,6 +73,37 @@ public class Graph1 {
         }
     }
 
+    public static void dfs(ArrayList<Edge> graph[] ,int curr , boolean vis[]){
+        System.out.print(curr+" ");
+        vis[curr] = true;
+
+        for (int i=0;i<graph[curr].size();i++){
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]){
+                dfs(graph,e.dest,vis);
+
+            }
+        }
+    }
+
+
+    public static boolean hasPath(ArrayList<Edge> graph[] ,int src,int dest,boolean vis[]){
+        if (src==dest){
+            return true;
+        }
+        vis[src]=true;
+        for (int i=0;i<graph[src].size();i++){
+            Edge e = graph[src].get(i);
+            if (!vis[e.dest] && hasPath(graph,e.dest,dest,vis)){
+                return true;
+            }
+
+        }
+        return false;
+
+
+    }
+
 
     public static void main(String[] args) {
 
@@ -113,6 +144,14 @@ public class Graph1 {
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
         bfs(graph);
+        System.out.println();
+        boolean vis[] = new boolean[V];
+
+        dfs(graph,0,vis);
+        System.out.println();
+        System.out.println(hasPath(graph,0,5,new boolean[V]));
+
+
     }
 
 }
