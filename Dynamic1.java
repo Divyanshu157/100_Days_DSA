354c4a14fb24a7efbb2a484ab54c58f911c300b7
divyanshu sharma <107141300+Divyanshu157@users.noreply.github.com>
2023-12-14 19:37:28 +0530

    Day51 basic Dynamic Code countways and fibonacci

---
+++ Dynamic1.java
@@ -0,0 +1,30 @@
+public class Dynamic1 {
+
+    public static int countWays(int n){
+        if (n==0){
+            return 1;
+        }
+        if (n<0){
+            return 0;
+        }
+        return  countWays(n-1)+countWays(n-2);
+
+    }
+    
+    //fiboonaci
+    public static int fib(int n){
+        if (n==0 || n==1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+
+    }
+
+    public static void main(String args[]){
+        int n=5;
+
+        System.out.println(countWays(5));
+
+
+    }
+}
