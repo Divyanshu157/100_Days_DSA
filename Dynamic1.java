bdc5cbb8c21bc19808e2a4dace43af1c374eedb5
divyanshu sharma <107141300+Divyanshu157@users.noreply.github.com>
2023-12-15 22:10:56 +0530

    Day52 CountwaysTab Problem with different approch

--- Dynamic1.java
+++ Dynamic1.java
@@ -12,6 +12,9 @@ public class Dynamic1 {
     }
     
     //fiboonaci
+
+
+
     public static int fib(int n){
         if (n==0 || n==1){
             return n;
@@ -20,10 +23,30 @@ public class Dynamic1 {
 
     }
 
+    //Day52
+
+    //Differernt approch fro ClimbStairs
+    public static int countWaysTab(int n){
+        int dp[] = new int [n+1];
+        dp[0]=1;
+        for (int i=1;i<=n;i++){
+            if (i==1){
+                dp[i]=dp[i-1];
+            }else {
+                dp[i]=dp[i-1]+dp[i-2];
+            }
+        }
+        return dp[n];
+
+    }
+
     public static void main(String args[]){
         int n=5;
 
         System.out.println(countWays(5));
+
+        //
+        System.out.println(countWaysTab(5));
 
 
     }
