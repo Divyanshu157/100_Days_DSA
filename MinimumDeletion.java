1237a025963d42674c2894b801ea444d21c0c13e
divyanshu sharma <107141300+Divyanshu157@users.noreply.github.com>
2024-01-03 02:13:11 +0530

    Day71 minimum deletion for palin. a string

--- MinimumDeletion.java
+++ MinimumDeletion.java
@@ -1,6 +1,25 @@
 public class MinimumDeletion {
 
+    public static int utility(String str1, int i ,int j){
+
+        if (i>=j) {
+            return 0;
+        }
+        if (str1.charAt(i)==str1.charAt(j)){
+                return utility(str1,i+1,j-1);
+        }
+        return 1+Math.min(utility(str1,i+1,j),utility(str1,i,j-1));
+    }
+
+    public static int min_del(String str1){
+        return utility(str1,0,str1.length()-1);
+
+    }
+
     public static void main(String[] args) {
-        
+
+        String str1 = "aebcbda";
+        System.out.println(min_del(str1));
+
     }
 }
