e35f2675a69cadde8eddac0a7516e200b91ad487
divyanshu sharma <107141300+Divyanshu157@users.noreply.github.com>
2024-01-06 23:03:09 +0530

    Day 74 space optimized aproch equalPartitionSum

--- PartitionEqualSubsetSum.java
+++ PartitionEqualSubsetSum.java
@@ -33,6 +33,42 @@ public class PartitionEqualSubsetSum {
         return equalPartition(arr, n, sum / 2);
     }
 
+    //different approch  space optimized approch
+
+
+    public static boolean findPartiion(int arr[], int n)
+    {
+        int sum = 0;
+        int i, j;
+
+        for (i = 0; i < n; i++)
+            sum += arr[i];
+
+        if (sum % 2 != 0)
+            return false;
+
+        boolean[] part = new boolean[sum / 2 + 1];
+
+
+        for (i = 0; i <= sum / 2; i++) {
+            part[i] = false;
+        }
+
+
+        for (i = 0; i < n; i++) {
+
+
+            for (j = sum / 2; j >= arr[i]; j--) {
+
+                if (part[j - arr[i]] == true || j == arr[i])
+                    part[j] = true;
+            }
+        }
+        return part[sum / 2];
+    }
+
+
+
     public static void main(String[] args) {
 
 //        int arr[] = new  int[5];
