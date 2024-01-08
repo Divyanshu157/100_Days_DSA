5482d8be84f55189f73688a16ef746138a5ac20a
divyanshu sharma <107141300+Divyanshu157@users.noreply.github.com>
2024-01-08 23:45:02 +0530

    Day76 NumberOfIsland

---
+++ NumberOfIsland.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberOfIsland {
+
+    private boolean isValid(int adjr ,int adjc ,int n,int m)
+    {
+        return adjr >= 0 && adjr < n && adjc < m;
+
+    }
+    public List<Integer>  numOfIslands(int n ,int m ,int [][] operators){
+        DisiontSet ds = new DisiontSet(n*m);
+
+        int vis[][] = new int[n][m];
+        int cnt =0;
+        List<Integer> ans  = new ArrayList<>();
+        int len = operators.length;
+        for (int i=0;i<len;i++){
+            int row = operators[i][0];
+            int col = operators[i][0];
+            if (vis[row][col]==1){
+                ans.add(cnt);
+                continue;
+            }
+            vis[row][col] =1;
+            cnt++;
+            int dr[]={-1,0,1,0};
+            int dc[] ={0,1,0,-1};
+            for (int ind=0;ind<4;ind++){
+                int adjr = row+dr[ind];
+                int adjc = col + dc[ind];
+                if (isValid(adjr,adjc,n,m)){
+                    if (vis[adjr][adjc]==1){
+                        int nodeNo = row*m+col;
+                        int adjNodeNo = adjr *m+adjc;
+                        if (ds.findPar(nodeNo) != ds.findPar(adjNodeNo)){
+                            cnt--;
+                            ds.unionBySize(nodeNo , adjNodeNo);
+
+                        }
+                    }
+                }
+            }
+            ans.add(cnt);
+
+        }
+        return ans;
+
+
+    }
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
