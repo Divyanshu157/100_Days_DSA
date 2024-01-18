780e5587c3ddebb50d509002969f23eadd4ad3a2
divyanshu sharma <107141300+Divyanshu157@users.noreply.github.com>
2024-01-18 21:20:27 +0530

    Day86 -> find n prnt subString in Str using backtr

--- Backtracking1.java
+++ Backtracking1.java
@@ -18,6 +18,28 @@ public class Backtracking1 {
 
     }
 
+//    day86
+
+
+
+    public static void findsubString(String str ,String ans, int i ){
+
+        //base case
+        if (i==str.length()){
+            if (ans.length()==0){
+                System.out.println("null");
+            }else {
+                System.out.println(ans);
+            }
+            return;
+        }
+
+
+        //recursion
+        findsubString(str,ans+str.charAt(i),i+1);
+        findsubString(str,ans,i+1);
+    }
+
     public static void print(int arr[]){
         for (int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
@@ -27,9 +49,12 @@ public class Backtracking1 {
     }
 
     public static void main(String[] args) {
-        int arr[] =new int[5];
-        changeArr(arr,0,1);
-        print(arr);
+//        int arr[] =new int[5];
+//        changeArr(arr,0,1);
+//        print(arr);
+
+        
+        findsubString("abc" , "",0);
 
     }
 }
